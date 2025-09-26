65eb928c2bbcdd298f0428f57a48446946573e33
denoc1 <176614645+denoc1@users.noreply.github.com>
2025-09-26 02:47:32 +0000

    template

---
+++ Lab5.java
@@ -0,0 +1,23 @@
+// Program header
+
+public class Lab5{
+
+//put your four methods here.  Remeber, each method should have header documentation
+
+
+
+
+
+    public static void main(String[] arg){
+
+        //code to ask user for two points
+        
+        
+        //calls to the four methods
+        
+        
+        //Display of the results
+
+    }// end main
+    
+}//end Lab5 Class
